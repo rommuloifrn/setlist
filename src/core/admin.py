57a09e1fe8076d138aab99911d7e0a09fa2bcb2e
Rômulo da Silva <roomulo.cavalkant@outlook.com>
2024-04-05 14:00:21 +0000

    register models in admin, group list page

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Group, GroupAssociation, Music, Setlist, Tag
 
 # Register your models here.
+
+admin.site.register(Group)
+admin.site.register(GroupAssociation)
+admin.site.register(Music)
+admin.site.register(Setlist)
+admin.site.register(Tag)
--- src/core/views.py
+++ src/core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Group
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def groups(request):
+    groups = Group.objects.all()
+    return render(request, "groups.html", {"groups":groups})
