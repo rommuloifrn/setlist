d165fd7e034113289c68bdc6486f35caec7602ba
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-04-04 21:33:03 -0300

    feat: completed tone list

--- src/core/models.py
+++ src/core/models.py
@@ -22,14 +22,42 @@ class GroupAssociation(models.Model):
 
 class Music(models.Model):
     class Tone(models.TextChoices):
+        DO = "C", "Dó maior"
         MINOR_DO = "Cm", "Dó menor"
-        # add missing ones
+        DO_SUS = "C#", "Dó sustenido"
+        DO_SUS_MINOR = "C#m", "Dó sustenido menor"
+        
+        RE = "D", "Ré"
+        RE_MINOR = "Dm", "Ré menor"
+        RE_SUS = "D#", "Ré sustenido"
+        RE_SUS_MINOR = "D#m", "Ré sustenido menor"
+        
+        MI = "E", "Mi"
+        MI_MINOR = "Em", "Mi menor"
+        
+        FA = "F", "Fá"
+        FA_MINOR = "Fm", "Fá menor"
+        FA_SUS = "F#", "Fá sustenido"
+        FA_SUS_MINOR = "F#m", "Fá sustenido menor"
+        
+        SOL = "G", "Sol"
+        SOL_MINOR = "Gm", "Sol menor"
+        SOL_SUS = "G#", "Sol sustenido"
+        SOL_SUS_MINOR = "G#m", "Sol sustenido menor"
+        
+        LA = "A", "Lá"
+        LA_MINOR = "Am", "Lá menor"
+        LA_SUS = "A#", "Lá sustenido"
+        LA_SUS_MINOR = "A#m", "Lá sustenido menor"
+        
+        SI = "B", "Si"
+        SI_MINOR = "Bm", "Si menor"
 
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     artist = models.CharField(max_length=50)
-    # tone
-    # lenght
+    tone = models.CharField(max_length=4, choices=Tone.choices, default=Tone.MI)
+    lenght = models.DurationField()
 
 class Setlist(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
